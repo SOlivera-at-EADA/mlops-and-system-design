1bcf80b393d3f1081ba303ca22d0cf40e285112f
Santiago Olivera Vaquero <santiago.oliverav@gmail.com>
2026-04-28 20:13:47 +0200

    Hello World Update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def hello_world():
-  print("Hello world")
+def hello_world(name):
+  print("Hello world. My name is " + name + " and I'm learning about version control!")
 
 
+hello_world("Santiago Olivera")  # Hello world, my name is John Smith and I'm learning about version control!
